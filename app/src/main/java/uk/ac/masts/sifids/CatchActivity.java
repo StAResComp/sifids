01eb5cd0a4e7736ad40d7ef8d413df05fb2d85d2
Patrick McCann <pgm5@st-andrews.ac.uk>
2018-02-15 17:01:36 +0000

    Email stored preferences as an attachment

--- app/src/main/java/uk/ac/masts/sifids/CatchActivity.java
+++ app/src/main/java/uk/ac/masts/sifids/CatchActivity.java
@@ -1,16 +1,29 @@
 package uk.ac.masts.sifids;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 
 public class CatchActivity extends AppCompatActivity {
 
@@ -28,10 +41,31 @@ public class CatchActivity extends AppCompatActivity {
                 Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","abc@gmail.com", null));
                 emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
                 emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
-                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"abc@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"pgm5@st-andrews.ac.uk"});
+                emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + createFileToSend().getAbsoluteFile()));
                 startActivity(Intent.createChooser(emailIntent, "Send email..."));
             }
         });
+    }
+
+    private File createFileToSend() {
+        File file = null;
+        try {
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "fish-1.txt");
+            FileWriter writer= new FileWriter(file);
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            Map<String,?> keys = sharedPrefs.getAll();
+            for(Map.Entry<String,?> entry : keys.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue().toString());
+            }
+            writer.close();
+            Toast.makeText(getBaseContext(), "Temporarily saved contents in " + file.getPath(), Toast.LENGTH_LONG).show();
+        }
+        catch (IOException e) {
+            Toast.makeText(getBaseContext(), "Unable create temp file. Check logcat for stackTrace", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        return file;
     }
 
     @Override
