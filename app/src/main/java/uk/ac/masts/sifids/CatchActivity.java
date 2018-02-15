3412ac06f6187552c0568160bdbcd33dbcc1da19
Patrick McCann <pgm5@st-andrews.ac.uk>
2018-02-15 15:27:11 +0000

    Send an email from the app

--- app/src/main/java/uk/ac/masts/sifids/CatchActivity.java
+++ app/src/main/java/uk/ac/masts/sifids/CatchActivity.java
@@ -1,6 +1,7 @@
 package uk.ac.masts.sifids;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,8 +25,11 @@ public class CatchActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","abc@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"abc@gmail.com"});
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
             }
         });
     }
