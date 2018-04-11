6a0196a4688871612ca8a13dc62bd06ab372bdb1
Patrick McCann <pgm5@st-andrews.ac.uk>
2018-04-11 16:13:20 +0100

    Android update, basic location identification

--- catch/src/main/java/uk/ac/masts/sifids/activities/Fish1FormsActivity.java
+++ catch/src/main/java/uk/ac/masts/sifids/activities/Fish1FormsActivity.java
@@ -1,9 +1,13 @@
 package uk.ac.masts.sifids.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +16,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.List;
 
@@ -26,10 +35,35 @@ public class Fish1FormsActivity extends AppCompatActivity {
     public static RecyclerView.Adapter adapter;
     List<Fish1Form> forms;
 
+    private FusedLocationProviderClient mFusedLocationClient;
+    protected Location mLastLocation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            //request permission
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Toast.makeText(getBaseContext(), "Current location is:  " + location.getLatitude() + "/" + location.getLongitude(), Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(getBaseContext(), "Location unknown ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
         setContentView(R.layout.activity_fish_1_forms);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
